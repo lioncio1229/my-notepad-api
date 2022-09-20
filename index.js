97dfde9f015b65ca0abd1733b1801b73f4f60583
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2022-09-20 16:22:10 +0800

    Add a homepage

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import connect from './src/services/connection.js';
 import notesRouter from './src/routes/notes.router.js';
+import fs from 'fs';
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -9,11 +10,15 @@ const app = express();
 const PORT = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT;
 
 app.use(cors());
+app.use(express.static('public'))
 app.use(express.urlencoded());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('<h1>MY NOTEPAD API</h1>');
+    fs.readFile('public/index.html', (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(data.toString());
+    });
 });
 
 app.use('/notes', notesRouter.router);
