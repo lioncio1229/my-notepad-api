300c50a6f4926eeec016ee52790807e11d789f17
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2022-12-26 19:58:56 +0800

    Add session middleware and mock network delay

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
 import express from 'express';
 import cors from 'cors';
+import sessions from 'express-session';
 import connect from './services/connection.js';
 import notesRouter from './routes/notes.router.js';
 import fs from 'fs';
 import dotenv from "dotenv";
 dotenv.config();
 
-const app = express();
-const PORT = process.env.NODE_ENV === 'development' ? 3000 : process.env.PORT;
+const {NODE_ENV, SECRET_KEY} = process.env;
 
-app.use(cors());
+const app = express();
+const PORT = NODE_ENV === 'development' ? 3000 : process.env.PORT;
+const oneDay = 1000 * 60 * 60 * 24;
+
+if(NODE_ENV === 'development')
+{
+    const delay = async (req, res, next) => {
+        await new Promise((resolve) => {
+            setTimeout(resolve, parseInt(900));
+        });
+        next();
+    };
+    app.use(delay);
+}
+
+const corsOptions = {
+    credentials: true,
+    origin: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.static('public'))
 app.use(express.urlencoded());
 app.use(express.json());
+
+const sessionObj = {
+    secret: SECRET_KEY,
+    name : 'mnp_id',
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+}
+
+if(NODE_ENV === 'production')
+{
+    app.set('trust proxy', 1);
+    sessionObj.cookie.secure = true;
+}
+
+app.use(sessions(sessionObj));
 
 app.get('/', (req, res) => {
     fs.readFile('public/index.html', (err, data) => {
